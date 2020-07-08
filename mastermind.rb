2fed14a5896abee5c137b16d398a3472ed2bf51b
2pavel <pawelnwz@gmail.com>
2020-07-08 21:22:18 +0200

    player can guess computer generated code, first actual commit

--- mastermind.rb
+++ mastermind.rb
@@ -13,16 +13,21 @@ class Game
 
   def play
     @board.set_code
+    p @board.code
     loop do
       @guess = @player.guess
-      
+      print @board.give_feedback(@guess)
+      print "\n"
+      @chances -= 1
+      break if @board.feedback.all? { |peg| peg == '|'.white } && @board.feedback.length == 4
     end
+    puts 'You win!'
   end
 end
 
 class Player
   def guess
-    puts 'Enter your guess (example: "red green red yellow")'
+    puts 'Enter your guess:'
     input = gets.chomp.split(' ')
 
     input
@@ -30,7 +35,7 @@ class Player
 end
 
 class Board
-  attr_reader :code
+  attr_reader :code, :feedback
   def initialize
     @possible_colors = %w[red green blue yellow black white]
     @code = []
@@ -40,9 +45,36 @@ class Board
     4.times { @code << @possible_colors.sample }
     @code
   end
+
+  def give_feedback(guess)
+    @feedback = []
+    @not_guessed = []
+    @code_scan = []
+
+    4.times do |i|
+      if guess[i] == @code[i]
+        @feedback << '|'.white
+        @code_scan[i] = nil
+      else
+        @not_guessed << guess[i]
+        @code_scan[i] = @code[i]
+      end
+    end
+    @not_guessed.each do |color|
+      @code_scan.each do |code_element|
+        if code_element == color
+          @feedback << '|'.red
+          color = 'is_somewhere'
+          @code_element = 'checked'
+        end
+      end
+    end
+    @feedback.join(' ')
+  end
 end
 
 puts "\nTry to guess the secret code of 4 colors! Separate the colors with space only."
+puts '(example: red green red yellow)'
 puts 'Keep in mind that you have 12 chances and duplicate colors are allowed' \
      "\nPOSSIBLE COLORS: " + 'red '.red + 'green '.green + 'blue '.blue +
      'yellow '.yellow + 'black'.black.on_white + ' ' +
@@ -50,8 +82,5 @@ puts 'Keep in mind that you have 12 chances and duplicate colors are allowed' \
 puts 'To exit the game type ' + 'exit'.red.on_white
 print "\n"
 
-#player = Player.new
-#board = Board.new
-#board.set_code
 game = Game.new
 game.play
