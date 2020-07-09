a4436eb3c93caae955a2cfedec9a372e70598f08
2pavel <pawelnwz@gmail.com>
2020-07-09 14:50:52 +0200

    cleaned up a bit, added losing functionality

--- mastermind.rb
+++ mastermind.rb
@@ -6,6 +6,7 @@ require 'pry'
 class Game
   def initialize
     @chances = 12
+    @result = 'win'
 
     @player = Player.new
     @board = Board.new
@@ -15,13 +16,18 @@ class Game
     @board.set_code
     p @board.code
     loop do
-      @guess = @player.guess
-      print @board.give_feedback(@guess)
-      print "\n"
+      puts @board.give_feedback(@player.guess)
       @chances -= 1
-      break if @board.feedback.all? { |peg| peg == '|'.white } && @board.feedback.length == 4
+      break if @board.win?
+
+      if @chances.zero?
+        @result = 'lost'
+        break
+      end
+      puts "You have #{@chances} chances remaining."
+      print "\n"
     end
-    puts 'You win!'
+    puts "You #{@result}!"
   end
 end
 
@@ -62,20 +68,24 @@ class Board
     end
     @not_guessed.each do |color|
       @code_scan.each do |code_element|
-        if code_element == color
-          @feedback << '|'.red
-          color = 'is_somewhere'
-          @code_element = 'checked'
-        end
+        next unless code_element == color
+
+        @feedback << '|'.red
+        color = 'is_somewhere'
+        @code_element = 'checked'
       end
     end
     @feedback.join(' ')
+  end
+
+  def win?
+    @feedback.all? { |peg| peg == '|'.white } && @feedback.length == 4
   end
 end
 
 puts "\nTry to guess the secret code of 4 colors! Separate the colors with space only."
 puts '(example: red green red yellow)'
-puts 'Keep in mind that you have 12 chances and duplicate colors are allowed' \
+puts 'Keep in mind that you have 12 chances and duplicate colors are allowed.' \
      "\nPOSSIBLE COLORS: " + 'red '.red + 'green '.green + 'blue '.blue +
      'yellow '.yellow + 'black'.black.on_white + ' ' +
      'white'.white.on_black
